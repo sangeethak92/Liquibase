e0836c2d01e261f6cdb0f64bedae9e3edb318b4e
sangeetha <sangeethak92@gmail.com>
2019-12-10 06:37:49 +0000

    modify the sql

--- src/main/db/precondition.sql
+++ src/main/db/precondition.sql
@@ -1,18 +1,19 @@
 --liquibase formatted sql
 
---changeset sang:13
+--changeset sang:13 
+--validCheckSum: 8:098f6bcd4621d373cade4e832627b4f6
 --preconditions onFail:HALT onError:HALT
 --precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM Staff
 
 insert into Staff values(1,"Bob","Adyar","biology",90);
 insert into Staff values(2,"Peter","thiruvanmiyur","chemistry",75);
 
-/*
 --changeset sang:14
+--validCheckSum: 3:098f6bcd4621d373cade4e832627s7p1
 --preconditions dbms type:oracle
 
 insert into Employee values(3,"Ram","Anitha","Navallur","chennai");
-*/
+
 
 /*
 --changeset sang:15
