55b9a87f75dda0b0a1e63ba29eceb4612d31c9ec
sangeetha <sangeethak92@gmail.com>
2019-12-10 06:02:14 +0000

    modify the precondition.sql

--- src/main/db/precondition.sql
+++ src/main/db/precondition.sql
@@ -1,10 +1,13 @@
+--liquibase formatted sql
+
 --changeset sang:13
---precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM Staff;
+--preconditions onFail:HALT onError:HALT
+--precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM Staff
 
 insert into Staff values(1,"Bob","Charles","Adyar","chennai");
 insert into Staff values(2,"Peter","Ashwin","thiruvanmiyur","chennai");
 
 --changeset sang:14
---precondition dbms type="oracle"
+--preconditions dbms type:oracle
 
 insert into Employee values(3,"Ram","Anitha","Navallur","chennai");
