93a70192b82fd0dde4d38f7b13f8306bafb2a656
sangeetha <sangeethak92@gmail.com>
2019-12-10 06:33:02 +0000

    modify the sql

--- src/main/db/precondition.sql
+++ src/main/db/precondition.sql
@@ -7,10 +7,12 @@
 insert into Staff values(1,"Bob","Adyar","biology",90);
 insert into Staff values(2,"Peter","thiruvanmiyur","chemistry",75);
 
+/*
 --changeset sang:14
 --preconditions dbms type:oracle
 
 insert into Employee values(3,"Ram","Anitha","Navallur","chennai");
+*/
 
 /*
 --changeset sang:15
