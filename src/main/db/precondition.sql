29db18ede1ec180e2bb726f19108e690401dd09e
sangeetha <sangeethak92@gmail.com>
2019-12-10 06:15:10 +0000

    modified the sql

--- src/main/db/precondition.sql
+++ src/main/db/precondition.sql
@@ -11,3 +11,11 @@ insert into Staff values(2,"Peter","thiruvanmiyur","chemistry",75);
 --preconditions dbms type:oracle
 
 insert into Employee values(3,"Ram","Anitha","Navallur","chennai");
+
+
+--changeset sang:15
+--preconditions onFail:HALT onError:HALT
+--precondition-sql-check expectedResult:1 SELECT COUNT(*) FROM Staff
+
+insert into Staff values(1,"jo","chrompet","biology",90);
+insert into Staff values(2,"karthik","central","chemistry",75);
